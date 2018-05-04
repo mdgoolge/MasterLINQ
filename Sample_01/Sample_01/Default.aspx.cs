3d4ead175df451abb145efc6f2713c1806deb0d2
a <a@a.com>
2018-05-04 10:28:07 +0800

    asdf

--- Sample_01/Sample_01/Default.aspx.cs
+++ Sample_01/Sample_01/Default.aspx.cs
@@ -11,7 +11,27 @@ namespace Sample_01
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!Page.IsPostBack)
+            {
+                LINQQueryData();
+            }
+        }
+        void LINQQueryData()
+        {
+            int[] datasource = new int[100];
+            for (int i = 0; i < 100; i++)
+            {
+                datasource[i] = i;
+            }
 
+            var query = from i in datasource
+                        where i < 10
+                        select i;
+
+            foreach (var item in query )
+            {
+                Response.Write(item.ToString() + "<br/>");
+            }
         }
     }
 }
