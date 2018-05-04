b33c6f2e04edc39dbfd8635323528479a8a67df6
a <a@a.com>
2018-05-04 13:29:07 +0800

    s

--- Sample_01/Sample_01/Default.aspx.cs
+++ Sample_01/Sample_01/Default.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,6 +15,13 @@ namespace Sample_01
             if (!Page.IsPostBack)
             {
                 LINQQueryData();
+                Response.Write("<br/>");
+
+                OldQuery();
+                Response.Write("<br/>");
+
+                LINQQuery();
+                Response.Write("<br/>");
             }
         }
         void LINQQueryData()
@@ -28,9 +36,50 @@ namespace Sample_01
                         where i < 10
                         select i;
 
-            foreach (var item in query )
+            foreach (var item in query)
             {
                 Response.Write(item.ToString() + "<br/>");
+            }
+        }
+        void OldQuery()
+        {
+            string[] datasource = new string[100];
+            for (int i = 0; i < 100; i++)
+            {
+                datasource[i] = "A" + i.ToString().PadLeft(2, '0');
+            }
+
+            ArrayList results = new ArrayList();
+            foreach (string s in datasource)
+            {
+                int index = Int32.Parse(s.Substring(1));
+                if (index % 2 == 0)
+                {
+                    results.Add(s);
+                }
+            }
+
+            foreach (var item in results)
+            {
+                Response.Write(item + "<br/>");
+            }
+        }
+        void LINQQuery()
+        {
+            string[] datasource = new string[100];
+            for (int i = 0; i < 100; i++)
+            {
+                datasource[i] = "A" + i.ToString().PadLeft(2, '0');
+            }
+
+            var results = from s in datasource
+                          let index = int.Parse(s.Substring(1))
+                          where index % 2 == 0
+                          select s;
+
+            foreach (var item in results)
+            {
+                Response.Write(item + "<br/>");
             }
         }
     }
