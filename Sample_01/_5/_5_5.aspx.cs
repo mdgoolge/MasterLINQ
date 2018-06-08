401eb3dde5a2516e506d40773cf2901e72c1d159
a <a@a.com>
2018-06-08 14:36:21 +0800

    s

---
+++ Sample_01/_5/_5_5.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.MobileControls;
+using System.Web.UI.WebControls;
+
+namespace _5
+{
+    public partial class _5_5 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //CountQuery();
+            //SumQuery();
+            //MaxQuery();
+            //AggregateQuery(); 
+            LongCountQuery();
+        }
+        void CountQuery()
+        {
+            List<UserInfo> users = new List<UserInfo>();
+            for (int i = 0; i < 10; i++)
+            {
+                users.Add(new UserInfo(i, "User0" + i.ToString(), "User0" + i.ToString() + "@web.com"));
+            }
+
+            int cout = users.Count(u => u.ID > 3 && u.ID < 8);
+            Response.Write("cout:" + cout.ToString() + "</br>");
+        }
+        void SumQuery()
+        {
+            List<UserInfo> users = new List<UserInfo>();
+            for (int i = 0; i < 10; i++)
+            {
+                users.Add(new UserInfo(i, "User0" + i.ToString(), "User0" + i.ToString() + "@web.com"));
+            }
+
+            int Sum = users.Sum(u => u.ID);
+            Response.Write("Sum:" + Sum.ToString() + "</br>");
+        }
+        void MaxQuery()
+        {
+            List<UserInfo> users = new List<UserInfo>();
+            for (int i = 0; i < 10; i++)
+            {
+                users.Add(new UserInfo(i, "User0" + i.ToString(), "User0" + i.ToString() + "@web.com"));
+            }
+
+            int Max = users.Max(u => u.ID);
+            Response.Write("Max:" + Max.ToString() + "</br>");
+        }
+
+        void AggregateQuery()
+        {
+            int[] ints = new int[100];
+            for (int i = 0; i < 100; i++)
+            {
+                ints[i] = i;
+            }
+            int aggregate = ints.Aggregate((a, b) => a + b);
+            Response.Write("aggregate:" + aggregate.ToString() + "</br>");
+        }
+        void LongCountQuery()
+        {
+            List<UserInfo> users = new List<UserInfo>();
+            for (int i = 0; i < 1000000; i++)
+            {
+                users.Add(new UserInfo(i, "User0" + i.ToString(), "User0" + i.ToString() + "@web.com"));
+            }
+
+            long count = users.LongCount(u => u.ID > 3);
+            Response.Write("longcout:" + count.ToString() + "</br>");
+        }
+    }
+}
--- Sample_01/_5_8/Default.aspx.cs
+++ Sample_01/_5_8/Default.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,9 @@ namespace _5_8
         protected void Page_Load(object sender, EventArgs e)
         {
             AsEnumerableQuery();
+            AsQueryable();
+            Cast();
+            ToDictionary(); ToLookup();
         }
         void AsEnumerableQuery()
         {
@@ -24,9 +28,85 @@ namespace _5_8
                          where u.Username.IndexOf("05") > -1
                          select u;
             Response.Write("AsEnumerable 的操作结果：");
-            foreach (var item in values )
+            foreach (var item in values)
             {
                 Response.Write(item.Username + ",");
+            }
+            Response.Write("<br/>");
+        }
+        void AsQueryable()
+        {
+            List<UserInfo> users = new List<UserInfo>();
+            for (int i = 0; i < 10; i++)
+            {
+                users.Add(new UserInfo(i % 2, "User0" + i.ToString(), "User0" + i.ToString() + "@web.com"));
+            }
+            var values = from u in users.AsQueryable<UserInfo>()
+                         where u.Username.IndexOf("01") > -1
+                         select u;
+            Response.Write("AsQueryable 的操作结果：");
+            foreach (var item in values)
+            {
+                Response.Write(item.Username + ",");
+            }
+            Response.Write("<br/>");
+        }
+        void Cast()
+        {
+            ArrayList ints = new ArrayList();
+            for (int i = 0; i < 100; i++)
+            {
+                ints.Add(i.ToString());
+            }
+            var values = from i in ints.Cast<string>()
+                         where i.IndexOf("2") > -1
+                         select i;
+            Response.Write("Cast 的操作结果：");
+            foreach (var item in values)
+            {
+                Response.Write(item + ",");
+            }
+            Response.Write("<br/>");
+        }
+        void ToDictionary()
+        {
+            List<UserInfo> users = new List<UserInfo>();
+            for (int i = 0; i < 10; i++)
+            {
+                users.Add(new UserInfo(i, "User0" + i.ToString(), "User0" + i.ToString() + "@web.com"));
+            }
+            var values = from u in users
+                         where u.ID < 7
+                         select u;
+            Dictionary<int, UserInfo> dic = values.ToDictionary(u => u.ID);
+            Response.Write("ToDictionary 的操作结果：");
+            foreach (var item in dic)
+            {
+                Response.Write(item.Value.Username + ",");
+            }
+            Response.Write("<br/>");
+        }
+        void ToLookup()
+        {
+            List<UserInfo> users = new List<UserInfo>();
+            for (int i = 1; i < 10; i++)
+            {
+                users.Add(new UserInfo(i, "User0" + i.ToString(), "User0" + i.ToString() + "@web.com"));
+            }
+            var values = from u in users
+                         where u.ID < 7
+                         select u;
+
+            ILookup<int, UserInfo> lookup = values.ToLookup(u => u.ID);
+
+            Response.Write("ToLookup 的操作结果：");
+            foreach (var lu in lookup)
+            {
+                foreach (var v in lu)
+                {
+                    Response.Write(v.Username + ",");
+                }
+
             }
             Response.Write("<br/>");
         }
